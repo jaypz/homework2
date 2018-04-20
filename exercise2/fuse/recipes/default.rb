4463d51639d1f316470ea704127f3a6fa412251a
johnpz <j.john.pease@gmail.com>
2018-04-20 10:58:22 -0400

    updated

--- exercise2/fuse/recipes/default.rb
+++ exercise2/fuse/recipes/default.rb
@@ -6,10 +6,19 @@
 #
 
 apt_package 'apache2' do
-	action: install
+	action :install
 end
 
 template '/var/www/html/fuse.html' do
 	mode '0644'
 	source 'fuse.erb'
+	action :create
+end
+
+cookbook_file '/var/www/html/.htaccess' do
+	source 'htaccess'
+	onwer 'root'
+	group 'www-data'
+	mode '0640'
+	action :create
 end
